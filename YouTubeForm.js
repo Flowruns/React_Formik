40feb49592c4d590b5926516e09ad18164d5932c
EKATERINA_ZAKHAROVA <93439877+Flowruns@users.noreply.github.com>
2024-05-06 21:12:34 +0700

    Arrays

--- YouTubeForm.js
+++ YouTubeForm.js
@@ -14,7 +14,8 @@ const initialValues = {
     social: {
         facebook: '',
         twitter: ''
-    }
+    },
+    phoneNumbers: ['', '']
 }
 
 /**
@@ -123,6 +124,22 @@ const YouTubeForm = () => {
                         type='text'
                         id='twitter'
                         name='social.twitter'
+                    />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='primaryPh'>Primary phone number</label>
+                    <Field
+                        type='text'
+                        id='primaryPh'
+                        name='phoneNumbers[0]'
+                    />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='secondaryPh'>Secondary phone number</label>
+                    <Field
+                        type='text'
+                        id='secondaryPh'
+                        name='phoneNumbers[1]'
                     />
                 </div>
                 <button type='submit'>Submit</button>
