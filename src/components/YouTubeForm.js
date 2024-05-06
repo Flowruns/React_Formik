7cca011f8375faea11d20c744cf4005c172b2ee1
Ekaterina Flowruns <mail@flowtuns.ru>
2024-05-06 16:45:52 +0700

    visited ields

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -77,19 +77,38 @@ const YouTubeForm = () => {
             <form onSubmit={formik.handleSubmit}>
                 <div className='form-control'>
                     <label htmlFor='name'>Name</label>
-                    <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
+                    <input 
+                        type='text' 
+                        id='name' 
+                        name='name' 
+                        onChange={formik.handleChange} 
+                        value={formik.values.name} 
+                        onBlur={formik.handleBlur}
+                    />
                     {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
                 </div>
                 <div className='form-control'>
                     <label htmlFor='email'>E-mail</label>
-                    <input type='email' id='email' name='email' onChange={formik.handleChange}
-                           value={formik.values.email}/>
+                    <input 
+                        type='email' 
+                        id='email' 
+                        name='email' 
+                        onChange={formik.handleChange}
+                        value={formik.values.email} 
+                        onBlur={formik.handleBlur}
+                    />
                     {formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
                 </div>
                 <div className='form-control'>
                     <label htmlFor='channel'>Channel</label>
-                    <input type='text' id='channel' name='channel' onChange={formik.handleChange}
-                           value={formik.values.channel}/>
+                    <input 
+                        type='text' 
+                        id='channel' 
+                        name='channel' 
+                        onChange={formik.handleChange}
+                        value={formik.values.channel} 
+                        onBlur={formik.handleBlur}
+                    />
                     {formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
                 </div>
                 <button type='submit'>Submit</button>
